3aba7cfb033b5bc51f3f790fcfb94b44812bef40
Vinko Roditi <vinko.d.roditi@gmail.com>
2022-04-28 12:27:49 +0200

    feature cards equipe

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 
    
     var calendarEl = document.getElementById('calendar');
-    let nomMonEvenement = ""
-    let datemonEvenement = ""
-    let eventsForCalendar = [
-
-        
-    ];
+    let eventsForCalendar = [];
     console.log(eventsForCalendar)
 
     //obtenir les ino depuis l'api
@@ -21,12 +16,46 @@ document.addEventListener('DOMContentLoaded', async function () {
             id: element.id,
             start: date,
             duration: element.heure_debut,
-            title: element.nom,
-           
-            
+            title: element.nom,     
         }
 
         eventsForCalendar.push(objectToPush)
+    }
+    let teamEl = document.getElementById("team")
+    let teamList = await getTeams()
+    for(const team of teamList) {
+        teamEl.innerHTML += 
+       
+                           ` <div class="card col" style="width: 18rem;">
+  <img src="${team.logo}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${team.nom}</h5>
+    <p class="card-text">Entraineur: ${team.entraineur}</p>
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#${team.nom}">
+  Liste des joueurs
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="${team.nom}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+  </div>
+</div> `
     }
     
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -37,14 +66,6 @@ document.addEventListener('DOMContentLoaded', async function () {
             right: 'today prev,next' // will normally be on the right. if RTL, will be on the left
           },
           events: eventsForCalendar
-          //[
-        //      { // this object will be "parsed" into an Event Object
-        //          initialView: 'dayGridMonth',
-        //          title: nomMonEvenement, // a property!
-        //          start: datemonEvenement // a property!
-        //          // end: '2022-04-28' // a property! ** see important note below about 'end' **
-        //      }
-        //  ]
     });
 
     calendar.render();
@@ -56,6 +77,16 @@ document.addEventListener('DOMContentLoaded', async function () {
 async function getEvents() {
     try {
         let response = await axios.get("https://apitournoi.nait-web.com/api/tournoi/list")
+        if (response.status !== 200) throw new Error('failed')
+        return response.data
+    } catch (e) {
+        console.log(e)
+    }
+}
+// get team
+async function getTeams() {
+    try {
+        let response = await axios.get("https://apitournoi.nait-web.com/api/equipe/list")
         if (response.status !== 200) throw new Error('failed')
         return response.data
     } catch (e) {
